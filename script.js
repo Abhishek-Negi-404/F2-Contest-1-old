30baa31ca1c34b236edddce1ff9590117abf10b8
Abhishek-Negi-404 <117743203+Abhishek-Negi-404@users.noreply.github.com>
2023-04-15 19:04:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,22 +6,60 @@ let arr = [
   { id: 3, name: "karen", age: "19", profession: "admin" },
 ];
 
+// Map Function------------------------------------------------------------------------------------------------------------------------------------------------
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map(printingThroghMap); // Each element is stored in "printingThroghMap" variable
+
+  function printingThroghMap(arrayItem) {
+    if (arrayItem.profession === "developer") {
+      console.log(arrayItem);
+    }
+  }
 }
 
+// For Each Function-------------------------------------------------------------------------------------------------------------------------------------------
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(printingThroghForEach); // Each element is stored in "printingThroghForEach" variable
+
+  function printingThroghForEach(arrayItem) {
+    if (arrayItem.profession === "developer") {
+      console.log(arrayItem);
+    }
+  }
 }
 
+// Add Data Function-------------------------------------------------------------------------------------------------------------------------------------------
 function addData() {
   //Write your code here, just console.log
+  let newObj = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newObj);
+  console.log(arr);
 }
 
+// Remove Admin Function---------------------------------------------------------------------------------------------------------------------------------------
 function removeAdmin() {
   //Write your code here, just console.log
+   let filtered_arr = arr.filter(function (val) {
+    //callback function
+    if (val.profession !== "admin") {
+      //filtering criteria
+      return val;
+    }
+  });
+  console.log(filtered_arr);
 }
 
+// Concatinate Function----------------------------------------------------------------------------------------------------------------------------------------
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr2 = [
+    { id: 5, name: "swapnilC", age: "30", profession: "PROgrammer" },
+    { id: 6, name: "akshayG", age: "28", profession: "developer" },
+    { id: 7, name: "vidhiJ", age: "21", profession: "Manager" },
+  ];
+
+  let concatArray = arr.concat(arr2);
+  console.log(concatArray);
 }
